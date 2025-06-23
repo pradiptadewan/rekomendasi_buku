e421106188acf27317c2953b7d998acf4004f875
pradiptadewan <pradiptadwn13@gmail.com>
2025-06-24 00:54:52 +0700

    deploy2

--- build.sh
+++ build.sh
@@ -2,14 +2,8 @@
 # exit on error
 set -o errexit
 
-# Perintah untuk menginstall semua perkakas dari daftar belanja
-pip install -r requirements.txt
+# Vercel akan otomatis menjalankan 'pip install' sebelum script ini.
+# Jadi kita hanya perlu menjalankan perintah spesifik Django.
 
-# Perintah untuk mengumpulkan semua file statis (CSS, JS, gambar) ke satu tempat
 python manage.py collectstatic --no-input
-
-# Perintah untuk menjalankan migrasi database
 python manage.py migrate
-
-# Perintah untuk mengisi database buku dari CSV (jika perlu dijalankan saat deploy)
-# python manage.py import_books
